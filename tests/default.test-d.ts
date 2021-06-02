d8dd9609b865d2ecd3257fe58d78db34852ad94c
Matt <carlotta.matt@gmail.com>
2021-06-02 15:03:51 -0700

    [skip-ci]: chore(package): Update package.json for scoped pkg

--- tests/default.test-d.ts
+++ tests/default.test-d.ts
@@ -1,29 +1,7 @@
 // import { expectType } from "tsd";
 // import utils from "@noshot/utils";
-// import type { ConfigOptions, ParsedEnvs, ProcessEnv } from "@noshot/utils";
 
-// const result = env.config();
+// const result = utils.;
 // expectType<string>(result.parsed["ROOT"]);
 
-// const { parsed, extracted } = env.config({
-//   dir: "tests",
-//   paths: ".env-example",
-//   encoding: "utf8",
-//   debug: true
-// });
 
-// expectType<ProcessEnv>(parsed);
-// expectType<string>(parsed["BASE"]);
-// expectType<ParsedEnvs>(extracted);
-// expectType<string>(extracted["BASE"]);
-
-// expectType<ConfigOptions>(env.load("test"));
-// const envConfig = env.load("test");
-// expectType<ConfigOptions>(envConfig);
-// expectType<string>(envConfig["dir"] as string);
-
-// expectType<ParsedEnvs>(env.parse("NODE_ENV=production\nDB_HOST=a.b.c"));
-
-// const parsedEnvs = env.parse(Buffer.from("JUSTICE=league\n"));
-// expectType<ParsedEnvs>(parsedEnvs);
-// expectType<ProcessEnv>(env.assign(parsedEnvs))
