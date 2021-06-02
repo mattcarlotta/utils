0522f3991eb2f7f6c8d8303180711e1bf84f73a3
Matt <carlotta.matt@gmail.com>
2021-06-02 15:25:10 -0700

    chore(release): Release v1.0.0

--- tests/default.test-d.ts
+++ tests/default.test-d.ts
@@ -1,7 +1,17 @@
-// import { expectType } from "tsd";
-// import utils from "@noshot/utils";
+import { expectType } from "tsd";
+import utils from "@noshot/utils";
 
-// const result = utils.;
-// expectType<string>(result.parsed["ROOT"]);
+(async () => {
+  expectType<void>(await utils.compressFiles(["tests/hello.js"]));
+  expectType<boolean>(utils.fileExists("tests/hello.js"));
+  expectType<string>(utils.getFilePath("hello.js", "tests"));
+  expectType<void>(await utils.removeFiles(["tests/hello.js"]));
+  expectType<void | Error>(await utils.waitFor(() => {
+    console.log("expectation")
+  }))
+  expectType<undefined>(await utils.waitForAct(() => {
+    console.log("expectation")
+  }))
+})()
 
 
